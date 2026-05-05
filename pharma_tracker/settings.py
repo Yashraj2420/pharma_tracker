b68e4c0a5435db7c995f02d6e31954111e519347
Yashraj <yashraj@Yashrajs-MacBook-Air.local>
2026-05-06 01:54:22 +0530

    fix static + allowed hosts

--- pharma_tracker/settings.py
+++ pharma_tracker/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,10 +23,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7#susq)oz*7%%*-qhatc1u1yt9^w+85mrp6!7@tj0w_zl=$e%a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.1.3','127.0.0.1']
-
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    'pharma-tracker-qprq.onrender.com',
+]
+CSRF_TRUSTED_ORIGINS = [
+    'https://pharma-tracker-qprq.onrender.com'
+]
 
 # Application definition
 
@@ -49,6 +55,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'pharma_tracker.urls'
@@ -117,5 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
