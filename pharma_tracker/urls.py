edb1e3c0430a980f09b0cd14a26033344133bb5f
Yashraj <yashraj@Yashrajs-MacBook-Air.local>
2026-06-11 17:48:59 +0530

    add demo data restore

--- pharma_tracker/urls.py
+++ pharma_tracker/urls.py
@@ -1,16 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import home, verify_batch
-from django.conf import settings
-from django.conf.urls.static import static
-from core.views import home, verify_batch, reset_admin
+from core.views import home, verify_batch, reset_admin, seed_demo_data
 
 urlpatterns = [
-    path('', home),
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
-    path('verify/<str:code>/', verify_batch),  
+    path('verify/<str:code>/', verify_batch, name='verify_batch'),
     path('reset-admin/', reset_admin),
+    path('seed-demo-data/', seed_demo_data),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
