606d195ae3f1f5ebffb047d6b43a2b71d40ef3f8
Yashraj <yashraj@Yashrajs-MacBook-Air.local>
2026-06-11 19:53:24 +0530

    add test url

--- pharma_tracker/urls.py
+++ pharma_tracker/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import home, verify_batch, reset_admin, seed_demo_data
+from core.views import home, verify_batch, reset_admin, seed_demo_data, qr_code, test_url
 
 urlpatterns = [
     path('', home, name='home'),
@@ -8,4 +8,6 @@ urlpatterns = [
     path('verify/<str:code>/', verify_batch, name='verify_batch'),
     path('reset-admin/', reset_admin),
     path('seed-demo-data/', seed_demo_data),
+    path('qr/<str:code>/', qr_code, name='qr_code'),
+    path('test-url/', test_url),
 ]
